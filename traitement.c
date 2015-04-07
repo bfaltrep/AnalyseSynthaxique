03956973f1424e823d5d7a773c59b3cb05f23d08
Amelie Guemon <aguemon@emi.u-bordeaux1.fr>
2015-04-07 16:53:04 +0200

    nettoyage des fonctions strcpy et strcat dans traitement.c #MerciMZ

--- traitement.c
+++ traitement.c
@@ -14,39 +14,36 @@ char * type_to_string(int type){
 
 /*Formate et ajoute un bloc associé a des selecteurs dans le css.*/
 void ajout_regles_css( char * selecteurs, char * regles){
-   //+5 pour '{}' et retour a la ligne. +1 : '\0'
-   char * res = malloc(sizeof(char * ) * (strlen(selecteurs)+strlen(regles)+5+1));
-   res = strcpy(res,selecteurs);
-   res = strcat(res, "{\n");
-   res = strcat(res,regles);
-   res = strcat(res, "}\n\n");
-   fprintf(flot_css,"%s",res);
-   free(res);
+   char *a ="{\n",
+     *b = "}\n\n";
+   fprintf(flot_css,"%s%s%s%s", selecteurs, a, regles, b);
 }
 
 void ajout_enTete_html (char * language, char * title){
    //2 : langage. 1 : \0. 37, 59, 15 : code preforme
-   char * res = malloc(sizeof(char * ) * (strlen(title)+2+1+111));
-   res = strcpy(res,"<head><meta charset=\"utf-8\" lang=\"");
-   res = strcat(res, language);
-   res = strcat(res, "\" /><link  rel=\"stylesheet\" href=\"style.css\" /><title>");
-   res = strcat(res, title);
-   res = strcat(res, "</title></head>");
-   fprintf(flot_html,"%s",res);
-   free(res);
+   char *a ="<head><meta charset=\"utf-8\" lang=\"",
+     *b = "\" /><link  rel=\"stylesheet\" href=\"style.css\" /><title>",
+     *c = "</title></head>";
+
+   fprintf(flot_html,"%s%s%s%s%s", a, language, b, title, c);
 }
 
 void ajout_balise_class(int type, char * contenu){
-   char * tmp = type_to_string(type);
+   /* char * tmp = type_to_string(type); */
    
-   char * res = malloc(sizeof(char * ) * (strlen(contenu)+strlen(tmp)+1+22));
-   res = strcpy(res,"<span class=\"");
-   res = strcat(res,tmp);
-   res = strcat(res, "\">");
-   res = strcat(res, contenu);
-   res = strcat(res, "</span>");
-   fprintf(flot_html,"%s",res);
-   free(res);
+   /* char * res = malloc(sizeof(char * ) * (strlen(contenu)+strlen(tmp)+1+22)); */
+   /* res = strcpy(res,"<span class=\""); */
+   /* res = strcat(res,tmp); */
+   /* res = strcat(res, "\">"); */
+   /* res = strcat(res, contenu); */
+   /* res = strcat(res, "</span>"); */
+   /* fprintf(flot_html,"%s",res); */
+   /* free(res); */
+
+   char * a = "<span class=\"",
+     *b = "\">",
+     *c = "</span>";
+   fprintf(flot_html, "%s%s%s%s%s", a, type_to_string(type), b, contenu, c);
 }
 
 
@@ -74,15 +71,12 @@ int create_files(){
    ajout_regles_css( ".type_specifier","color : #AAAAAA;\n");
    //ajouter regle pour nomfonction+nomvariable avec une pile.
    //ajout_regles_css( "class=\"type_specifier\" ","color : #AAAAAA;\n");
-
-   
-   buf = "</body></html>";
-   fprintf(flot_html,"%s",buf);
-   ajout_regles_css( "h1","color : #8291CF;\n");
    return 0;
 }
 
 void finish(){
-   fclose(flot_html); 
-   fclose(flot_css); 
+  char * buf = "</body></html>";
+  fprintf(flot_html,"%s",buf);
+  fclose(flot_html); 
+  fclose(flot_css); 
 }
